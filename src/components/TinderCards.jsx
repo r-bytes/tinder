dd11fe32254c01105269a6ab8ed4dd9da9363c7b
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-09-30 17:08:35 +0200

    Created TinderCards and Style

---
+++ src/components/TinderCards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TinderCardStyle } from "../styles"
+
+const TinderCards = () => {
+  return (
+    <div className="tinderCards">
+        TinderCards
+    </div>
+  )
+}
+
+export default TinderCards
