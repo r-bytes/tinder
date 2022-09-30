78a55e18fc3db0dfd45dc345751d965482ab3f09
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-09-30 17:08:11 +0200

    Created SwipeButtons and Style

---
+++ src/components/SwipeButtons.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SwipeButtonStyle } from "../styles"
+
+const SwipeButtons = () => {
+  return (
+    <div className="swipeButtons">
+        SwipeButtons
+    </div>
+  )
+}
+
+export default SwipeButtons
